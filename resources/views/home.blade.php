e4b4ea89943c4617bdc6f05596a0e8541c7d45a9
Pablo Câmara <pablo.camara.pt@gmail.com>
2023-04-16 11:14:01 +0100

    added container elements for upcoming react components

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-<div id="events-user-has-been-invited-to"></div>
+<div id="highlighted-events"></div>
+<div id="next-events"></div>
+<div id="events-happening-right-now"></div>
+<div id="past-events"></div>
 @endsection
