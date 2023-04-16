e769e18739fc0505cd6ad2b3b36dec999b9d5446
Pablo Câmara <pablo.camara.pt@gmail.com>
2023-04-16 11:27:47 +0100

    added 'Highlighted Events' react component

---
+++ resources/js/components/HighlightedEvents.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { trans } from '../helpers/translation';
+
+function HighlightedEvents() {
+    return (
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-md-8">
+                    <div className="card">
+                        <div className="card-header">{trans('Highlighted events')}</div>
+
+                        <div className="card-body">I'm an example component!</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default HighlightedEvents;
+
+if (document.getElementById('highlighted-events')) {
+    ReactDOM.render(<HighlightedEvents />, document.getElementById('highlighted-events'));
+}
