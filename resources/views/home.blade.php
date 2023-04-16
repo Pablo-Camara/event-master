000788d5ff1223af5db83d22d27b7cb3375f30c8
Pablo Câmara <pablo.camara.pt@gmail.com>
2023-04-16 11:50:48 +0100

    showing events the logged in user had been invited to

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
+@auth
+<div id="events-user-has-been-invited-to"></div>
+@endauth
 <div id="highlighted-events"></div>
 <div id="next-events"></div>
 <div id="events-happening-right-now"></div>
