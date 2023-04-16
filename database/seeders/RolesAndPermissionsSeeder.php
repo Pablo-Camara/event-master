910e8a44e6c4593efae6d970c2bd648eb45a627e
Pablo Câmara <pablo.camara.pt@gmail.com>
2023-04-16 15:53:45 +0100

    created seeder for 'admin' role and 'manage users' permission

---
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class RolesAndPermissionsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $role = Role::create(['name' => 'admin']);
+        $permission = Permission::create(['name' => 'manage users']);
+        $role->givePermissionTo($permission);
+
+        $this->command->info('"admin" role and "manage users" permission created successfully!');
+    }
+}
