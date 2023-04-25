40a040f13733dbf4730bb5487f7a55cb4db31b4b
Pablo Câmara <pablo.camara.pt@gmail.com>
2023-04-25 06:21:01 +0100

    created EventOccurrenceLocation model

---
+++ app/Models/EventOccurrenceLocation.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class EventOccurrenceLocation extends Model
+{
+    use HasFactory;
+}
