872613fdc7e15c6455c8e5c28948dd8b5e2873b7
Pablo Câmara <pablo.camara.pt@gmail.com>
2023-04-25 04:31:17 +0100

    clean up home controller now that we have a view composer

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,9 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\EventOccurrence;
-use Carbon\Carbon;
-
 class HomeController extends Controller
 {
     /**
@@ -14,23 +11,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $currentDate = Carbon::now();
-        $formattedDate = $currentDate->format('Y-m-d');
-        $formattedTime = $currentDate->format('H:i:s');
-
-
-        $nextHighlightedOccurrences = EventOccurrence::whereHas('event', function ($query) {
-            $query->where('highlighted', true);
-        })->with('event')
-        ->where('occurrence_date', '=', $formattedDate)
-        ->where('start_time', '>', $formattedTime)
-        ->orderBy('start_time')
-        ->get();
-
-        $events = [
-            'highlighted' => $nextHighlightedOccurrences
-        ];
-
-        return view('home', compact('events'));
+        return view('home');
     }
 }
