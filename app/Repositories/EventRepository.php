d216f6aee6e74d1c6cf90a305167b1e05175f81d
Pablo Câmara <pablo.camara.pt@gmail.com>
2023-04-25 04:17:39 +0100

    created event repository with getNextHighlightedEventOccurrences function

---
+++ app/Repositories/EventRepository.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\EventOccurrence;
+use Carbon\Carbon;
+
+class EventRepository
+{
+    public function getNextHighlightedEventOccurrences()
+    {
+        $currentDate = Carbon::now();
+        $formattedDate = $currentDate->format('Y-m-d');
+        $formattedTime = $currentDate->format('H:i:s');
+
+        $result = [];
+
+        try {
+            return EventOccurrence::whereHas('event', function ($query) {
+                $query->where('highlighted', true);
+            })->with('event')
+            ->where('occurrence_date', '=', $formattedDate)
+            ->where('start_time', '>', $formattedTime)
+            ->orderBy('start_time')
+            ->get();
+        } catch (\Throwable $th) {
+            // @TODO: capture exception for ex with sentry
+            // for now we throw the exception anyways:
+            throw $th;
+        }
+
+        return $result;
+    }
+}
